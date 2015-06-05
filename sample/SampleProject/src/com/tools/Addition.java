6392046613dc57277f65d9f41fe947e9d3a3a0d3
Puja_Deep <Puja_Deep@BLRKEC370352D.ad.infosys.com>
2015-06-05 10:56:07 +0530

    Added a statement to print the difference

--- sample/SampleProject/src/com/tools/Addition.java
+++ sample/SampleProject/src/com/tools/Addition.java
@@ -4,8 +4,9 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 5, b = 6;
+		int a = 8, b = 6;
 		System.out.println("Sum of the two numbers is " + (a + b));
+		System.out.println("Difference between the two numbers is "+ (a-b));
 	}
 
 }
