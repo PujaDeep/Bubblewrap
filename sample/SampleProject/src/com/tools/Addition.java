670b628f3ffe06d56dec162418e4d709b6867b78
Puja_Deep <Puja_Deep@BLRKEC370352D.ad.infosys.com>
2015-06-05 10:53:36 +0530

    Committing the base code.

---
+++ sample/SampleProject/src/com/tools/Addition.java
@@ -0,0 +1,11 @@
+package com.tools;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5, b = 6;
+		System.out.println("Sum of the two numbers is " + (a + b));
+	}
+
+}
